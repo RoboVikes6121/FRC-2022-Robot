531b44ce02087c13304bf27424ab5e32e980d352
janetmcbride <35512043+janetmcbride@users.noreply.github.com>
2022-02-24 15:32:59 -0500

    2/24/22 fist github commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,24 +1,77 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*
+//WPILi
+  2022 everybot code
+  written by carson graf 
+  don't email me, @ me on discord
+*/
+
+/*
+  This is catastrophically poorly written code for the sake of being easy to follow
+  If you know what the word "refactor" means, you should refactor this code
+*/
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import javax.lang.model.util.ElementScanner6;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+//new import
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.SPI;
+
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  //Definitions for the hardware. Change this if you change what stuff you have plugged in
+  TalonSRX driveLeftA = new TalonSRX(3);
+  TalonSRX driveLeftB = new TalonSRX(4);
+  TalonSRX driveRightA = new TalonSRX(1);
+  TalonSRX driveRightB = new TalonSRX(2);
+  double speedMultiplier = 0.7;
+  double RightAdjust = 0.73;
+  Timer m_timer = new Timer();
+  TalonFX arm = new TalonFX(5);
+  CANSparkMax intake = new CANSparkMax(6, MotorType.kBrushless);
+  private static final double kAngleSetpoint = 0.0;
+	private static final double kP = 0.005; // propotional turning constant
+
+   
+
+
+  // private final DifferentialDrive m_robotDrive = new DifferentialDrive(driveLeftA, driveRightA);
+  Joystick operator = new Joystick(1);
+  XboxController driver = new XboxController(0);
+
+  //Constants for controlling the arm. consider tuning these for your particular robot
+  final double armHoldUp = 0.08;
+  final double armHoldDown = 0.13;
+  final double armTravel = 0.5;
+
+  final double armTimeUp = 0.5;
+  final double armTimeDown = 0.35;
+
+  //Varibles needed for the code
+  boolean armUp = true; //Arm initialized to up because that's how it would start a match
+  boolean burstMode = false;
+  double lastBurstTime = 0;
+
+  double autoStart = 0;
+  boolean goForAuto = false;
+
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -26,73 +79,221 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    //Configure motors to turn correct direction. You may have to invert some of your motors
+    driveLeftA.setInverted(false);
+    driveLeftB.setInverted(false);
+    driveRightA.setInverted(false);
+    driveRightB.setInverted(true);
+    driveLeftB.follow(driveLeftA);
+    driveRightB.follow(driveRightA);
+    arm.setInverted(false);
+   // arm.setIdleMode(IdleMode.kBrake);
+    //arm.burnFlash();
+    driveRightA.configClearPositionOnLimitF(false, 0);
+    driveRightA.configClearPositionOnLimitR(false, 0);
+    driveRightA.configClearPositionOnQuadIdx(false, 0);
+    driveLeftA.configClearPositionOnLimitF(false, 0);
+    driveLeftA.configClearPositionOnLimitR(false, 0);
+    driveLeftA.configClearPositionOnQuadIdx(false, 0);
+    // set to false to track position
+    // set to true to stop tracking and reset to 0
+    // tedious
+    
+    //add a thing on the dashboard to turn off auto if needed
+    SmartDashboard.putBoolean("Go For Auto", false);
+    goForAuto = SmartDashboard.getBoolean("Go For Auto", false);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    
+    m_timer.reset();
+    m_timer.start();
+    driveLeftA.setSelectedSensorPosition(0);
+    driveRightA.setSelectedSensorPosition(0);
   }
-
+ 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+  public void autonomousPeriodic() { 
+    /**drive to goal
+     release ball
+    backup 
+     180
+     intake down 
+    driveLeftA.configClearPositionOnLimitF(false, 0);
+    driveRightA.configClearPositionOnLimitF(false, 0);
+    if (m_timer.get() < 5){
+      driveForward(0);
+  } else if(m_timer.get() > 5 & m_timer.get() < 6.2){
+    driveForward(.3);
+   }
+   else if(m_timer.get() > 6.2 & m_timer.get() < 6.5){
+     turnRight(.3);
+   }
+   else if(m_timer.get() > 6.5 & m_timer.get() < 9){
+     driveForward(0);
+   }
+   else if(m_timer.get() > 9 & m_timer.get() < 10){
+     driveForward(-.4);
+   }
+  else if(m_timer.get() > 10 & m_timer.get() < 12){
+     turnLeft(.4);
+  }
+   else if(m_timer.get() > 6 & m_timer.get() < 9){
+     driveForward(.3);
+   }
+   else{
+    driveForward(0);
+  }
+   if(m_timer.get() < 5){
+    driveForward(0);
+  }
+  else if(m_timer.get() > 6.2 & m_timer.get() < 6.5){
+    turnLeft(.3);
+  }
+  else if(m_timer.get() > 6.5 & m_timer.get() < 9){
+    driveForward(0);
+  }
+  else if(m_timer.get() > 9 & m_timer.get() < 10){
+    driveForward(-.4);
+  }
+ else if(m_timer.get() > 10 & m_timer.get() < 12){
+    turnLeft(.4);
+ }
+  else if(m_timer.get() > 6 & m_timer.get() < 9){
+    driveForward(.3);
+  }
+  else{
+   driveForward(0);
+    SmartDashboard.putNumber("left pos", driveLeftA.getSelectedSensorPosition());
+    SmartDashboard.putNumber("right pos", driveRightA.getSelectedSensorPosition());*/
+  }
+   
+
+  private void driveLeftA(double d) {
   }
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    driveLeftB.follow(driveLeftA);
+    driveRightB.follow(driveRightA);
+  }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    //Set up arcade steer
+    // double forward = -operator.getRawAxis(1);
+    // double turn = -operator.getRawAxis(2);
+		// Invert the direction of the turn if we are going backwards
+		//turningValue = Math.copySign(turningValue, driver.getLeftY());
+    double forward = 0;
+    double turn = 0;
+    // if(driver.getLeftX() < .1 && driver.getLeftX() > -.1){
+      // turn = 0;
+    // }
+    // else if(driver.getLeftY() <.1 && driver.getLeftY() > -.1){
+      // forward = 0;
+    // }
+    // else{
+    forward = -driver.getLeftY();
+    turn = -driver.getRightX()*speedMultiplier;
+  // }
+    
+    double driveLeftPower = forward*speedMultiplier - turn;
+    double driveRightPower = forward*RightAdjust + turn;
+   
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+    driveLeftA.set(ControlMode.PercentOutput, driveLeftPower);
+    //driveLeftB.set(ControlMode.PercentOutput, driveLeftPower);
+    driveRightA.set(ControlMode.PercentOutput, driveRightPower);
+    // driveRightB.set(ControlMode.PercentOutput, driveRightPower);
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+    //Intake controls
+     if(operator.getRawButton(11)){
+    //  if(driver.getAButton()){
+      intake.set(.3);
+      arm.set(ControlMode.PercentOutput, .3);
+      
+      //armUp = false;
+    }
+     //else if(operator.getRawButton(13)){
+    //  else if(driver.getBButton()){
+      //intake.set(0.3);
+    //}
+    //else{
+      //intake.set(0.3);
+    //}
 
-  /** This function is called periodically during test mode. */
+    //Arm Controls
+    if(armUp){
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp){
+        arm.set(ControlMode.Position,armHoldUp);
+      }
+      else{
+        arm.set(ControlMode.Position,armHoldUp);
+      }
+    }
+    else{
+      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown){
+        arm.set(ControlMode.Position,-armTravel);
+      }
+      else{
+        arm.set(ControlMode.Position,-armHoldDown);
+      }
+    }
+  
+     if(operator.getRawButtonPressed(14) && !armUp){
+    //  if(driver.getYButton()){
+      lastBurstTime = Timer.getFPGATimestamp();
+      armUp = true;
+    }
+     else if(operator.getRawButtonPressed(16) && armUp){
+    //  else if(driver.getXButton()){
+      lastBurstTime = Timer.getFPGATimestamp();
+      armUp = false;
+    }  
+
+  }
+
   @Override
-  public void testPeriodic() {}
+  public void disabledInit() {
+    //On disable turn off everything
+    //done to solve issue with motors "remembering" previous setpoints after reenable
+    driveLeftA.set(ControlMode.PercentOutput, 0);
+    driveLeftB.set(ControlMode.PercentOutput, 0);
+    driveRightA.set(ControlMode.PercentOutput, 0);
+    driveRightB.set(ControlMode.PercentOutput, 0);
+    arm.set(ControlMode.Position,0);
+    intake.set(0);
+  }
+  public void driveForward(double power){
+
+
+    driveLeftA.set(ControlMode.PercentOutput, power);
+    driveRightA.set(ControlMode.PercentOutput, power);
+}
+  
+  public void turnLeft(double power){
+
+    driveLeftA.set(ControlMode.PercentOutput, -power);
+    driveRightA.set(ControlMode.PercentOutput, power);
+
+  }
+  public void turnRight(double power){
+
+    driveLeftA.set(ControlMode.PercentOutput, power);
+    driveRightA.set(ControlMode.PercentOutput, -power);
+  }
+  public void forwardTurnLeft(double power){
+    driveLeftA.set(ControlMode.PercentOutput, -power);
+    driveRightA.set(ControlMode.PercentOutput, +power);
+  }
+  public void forwardTurnRight(double power){
+    driveLeftA.set(ControlMode.PercentOutput, +power);
+    driveRightA.set(ControlMode.PercentOutput, -power);
+  }
 }
